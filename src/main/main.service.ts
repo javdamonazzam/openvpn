a7dc77f0f2145c7347ab94e02526dfa785095779
javdamonazzam <m131037@gmail.com>
2025-04-28 23:13:48 +0330

    Update main.service.ts

--- src/main/main.service.ts
+++ src/main/main.service.ts
@@ -6,27 +6,14 @@ import * as fs from 'fs';  // برای کار با فایل
 export class MainService {
   async activateUser(publicKey: string): Promise<string> {
     const wg0ConfigPath = `/etc/openvpn/ccd/${publicKey}`;  // مسیر فایل کانفیگ WireGuard
-    return new Promise((resolve, reject) => {
-      fs.promises.readFile(wg0ConfigPath, 'utf-8')
-        .then(config => {
-          const updatedConfig = config.replace(
-            new RegExp(`# ### Client ${publicKey}[\\s\\S]*?# AllowedIPs = [\\d\\.]+/32`, 'g'),
-            match => match.split('\n').map(line => line.replace(/^# /, '')).join('\n')
-          );
-
-          return fs.promises.writeFile(wg0ConfigPath, updatedConfig, 'utf-8');
-        })
-        .then(() => {
-          exec('sudo systemctl restart wg-quick@wg0', (error, stdout, stderr) => {
-            if (error) {
-              reject(`Error restarting WireGuard: ${error.message}`);
-            } else {
-              resolve('User activated successfully');
-            }
-          });
-        })
-        .catch(err => reject(`Error handling config file: ${err.message}`));
-    });
+  
+    try {
+      await fs.promises.writeFile(wg0ConfigPath, '', 'utf-8');
+      return `User ${publicKey} has been deactivated.`;
+    } catch (error) {
+      console.error(error);
+      throw new Error(`Failed to deactivate user ${publicKey}: ${error}`);
+    }
   }
   async deactivateUser(publicKey: string): Promise<string> {
     const wg0ConfigPath = `/etc/openvpn/ccd/${publicKey}`;
